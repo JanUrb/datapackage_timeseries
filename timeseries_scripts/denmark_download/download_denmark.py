a00001762db241d52d3b563a2de7eb6e4d7e6a3d
JanUrb <JanUrb@users.noreply.github.com>
2016-08-01 18:25:56 +0200

    Revert "Revert "Download of the Danish xls file.""
    This reverts commit 0bbb05c6b75c58929024fb3dfcda42b38969a3b1.

--- timeseries_scripts/denmark_download/download_denmark.py
+++ timeseries_scripts/denmark_download/download_denmark.py
@@ -1,16 +1,24 @@
+__author__ = 'Jan'
+"""
+To download the xls for Denmark, just call the download_xls_file() in the notebook.
+"""
 import requests
 from bs4 import BeautifulSoup
 import datetime
 import os
+import logging
 
-# import logging
-# TODO: Comments, make it callable from a notebook, logging
+log = logging.getLogger('download_denmark')
+log.setLevel(logging.INFO)
+
+
 _TARGET_URL = r'http://www.energinet.dk/en/el/engrosmarked/udtraek-af-markedsdata/Sider/default.aspx'
 _REQUEST_URL = r'http://www.energinet.dk/_layouts/Markedsdata/Framework/Integrations/MarkedsdataExcelOutput.aspx'
-_FILE_NAME = 'danish.xls'
+_POST_PARAMETER_FILE = 'post_parameter.txt'
 
 
 def _extract_dotNet_variables():
+    log.debug('Extracting dotNet variables')
     s = requests.Session()
     r = s.get(_TARGET_URL)
     if r.status_code != 200:
@@ -25,53 +33,85 @@ def _extract_dotNet_variables():
 
 
 def _construct_parameter(view_state, event_validation, file_path):
+    log.debug('Constructing post parameter')
     parameter = {}
     with open(file_path, 'r') as para_file:
         lines = para_file.read().splitlines()
-        # 2nd last element is endDate. It has to be changed to current date
+        # last element is endDate. It has to be changed to current date
         current_time = datetime.datetime.now()
         current_date = current_time.strftime("%d-%m-%Y")
-        print(current_date)
-        lines[-2] = 'endDate=' + current_date
+        lines[-1] = 'endDate=' + current_date
         # split lines into dictionary
         for key_value in lines:
             try:
                 key, value = key_value.split('=')
                 parameter[key] = value
             except:
-                print('Error: ' + key_value)
+                log.warning('Error: ' + key_value)
 
         # add view_state and event_validation
         parameter['__VIEWSTATE'] = view_state
         parameter['__EVENTVALIDATION'] = event_validation
+        log.debug(parameter['startDate'])
+        log.debug(parameter['endDate'])
 
         return parameter
 
 
 def _download_excel(parameter, session, output_path):
-    post = session.post(_REQUEST_URL, data=parameter)
+    """
+    This function downloads the xls file from the website. To download, you have to send a post request with parameters
+    that specify the data you want first. After that you request the xls/html file.
+
+    Parameters
+    ----------
+    parameter : Specifies the data (ASP.NET Web Form)
+    session : The session that was used for extracting the variables
+    output_path : Defines the location for the downloaded xls file
+
+    Returns
+    -------
+
+    """
+    log.info('Downloading xls file.')
+    # Specifies the data you want.
+    session.post(_REQUEST_URL, data=parameter)
     # get excel
     header = {
         'referer': r'http://www.energinet.dk/_layouts/Markedsdata/framework/integrations/markedsdatatemplate.aspx?language=en',
-        'content-type': 'application/vnd.ms-excel; charset=utf-8'
-    }
-    r = session.get(_REQUEST_URL, stream=True, headers=header)
-    print('_download_excel: get-request' + str(r.status_code))
-    print('headers:')
-    print(r.headers)
+        'content-type': 'application/vnd.ms-excel; charset=utf-8',
+        'accept-encoding': 'gzip, deflate',
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'
 
+    }
+    # Gets the data you want.
+    r = session.get(_REQUEST_URL, stream=True, headers=header)
     with open(output_path, 'wb') as out_file:
         for chunck in r.iter_content(chunk_size=1024):
             out_file.write(chunck)
 
 
-if __name__ == '__main__':
-    # get file path
-    loc = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
-    para_path = os.path.join(loc, 'post_parameter.txt')
-    output_path = os.path.join(loc, _FILE_NAME)
-    print(output_path)
+def download_xls_file(output_directory='', output_file_name='danish.xls'):
+    """
+    Prepares the parameters and downloads the xls file.
+    Parameters
+    ----------
+    output_directory : Directory where the xls file is saved.
+    output_file_name : Name of the downloaded file.
 
+    Returns
+    -------
+
+    """
+    loc = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+    para_path = os.path.join(loc, _POST_PARAMETER_FILE)
+    output_path = os.path.join(output_directory, output_file_name)
     view, event, session = _extract_dotNet_variables()
     parameter_dict = _construct_parameter(view, event, para_path)
     _download_excel(parameter_dict, session, output_path)
+
+
+if __name__ == '__main__':
+    log.addHandler(logging.StreamHandler())
+    log.setLevel(logging.DEBUG)
+    download_xls_file()
